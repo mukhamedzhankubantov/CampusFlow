18c81d9d658b4649cf520194899fe074c3d2fb7d
AdllYul <adil.d.yul@gmail.com>
2026-09-12 13:15:20 +0500

    fix: Fixing bugs and create methods

--- src/main/java/com/campusflow/domain/CourseRegistration.java
+++ src/main/java/com/campusflow/domain/CourseRegistration.java
@@ -6,9 +6,36 @@ public class CourseRegistration {
     private final String courseId;
     private CourseRegistrationStatus status;
 
-    private final CourseRegistrationPolicy = new CourseRegistrationPolicy();
+    private final CourseRegistrationPolicy policy = new CourseRegistrationPolicy();
     public CourseRegistration(CourseRegistrationId id, String studentId, String courseId){
-
+        this.id = Objects.requireNonNull(id, "id cannot be null");
+        this.studentId = Objects.requireNonNull(studentId,"studentId cannot be null");
+        this.courseId = Objects.requireNonNull(courseId, "courseId cannot be null");
+        this.status = CourseRegistrationStatus.DRAFT;
     }
 
+    public void submit(){
+        policy.validateTransition(this.status, CourseRegistrationStatus.SUBMITTED);
+        this.status = CourseRegistrationStatus.SUBMITTED;
+    }
+    public void approve(){
+        policy.validateTransition(this.status, CourseRegistrationStatus.APPROVED);
+        this.status = CourseRegistrationStatus.APPROVED;
+    }
+    public void reject(){
+        policy.validateTransition(this.status, CourseRegistrationStatus.REJECTED);
+        this.status = CourseRegistrationStatus.REJECTED;
+    }
+    public CourseRegistrationId getId(){
+        return id;
+    }
+    public String getStudentId(){
+        return studentId;
+    }
+    public String getCourseId(){
+        return courseId;
+    }
+    public CourseRegistrationStatus getStatus(){
+        return status;
+    }
 }

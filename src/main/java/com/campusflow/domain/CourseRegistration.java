b5b1aad1fd771cd6e95b5731d9ede4df9b0bb4f9
AdllYul <adil.d.yul@gmail.com>
2026-09-12 10:22:11 +0500

    feat: created class CourseRegistration and defined fields

---
+++ src/main/java/com/campusflow/domain/CourseRegistration.java
@@ -0,0 +1,14 @@
+package com.campusflow.domain;
+import java.util.Objects;
+public class CourseRegistration {
+    private final CourseRegistrationId id;
+    private final String studentId;
+    private final String courseId;
+    private CourseRegistrationStatus status;
+
+    private final CourseRegistrationPolicy = new CourseRegistrationPolicy();
+    public CourseRegistration(CourseRegistrationId id, String studentId, String courseId){
+
+    }
+
+}
